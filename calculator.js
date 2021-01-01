3032f9f77c2eb405f322421d3a222eb61e921950
courtsideKB <kbankhead1992.yahoo.com>
2020-12-31 17:07:29 -0800

    Added body-parser in order to get data sent back from server

--- calculator.js
+++ calculator.js
@@ -2,13 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", (req,res) => {
+app.post("/", (req, res) => {
     var num1 = Number(req.body.num1);
     var num2 = Number(req.body.num2);
 
