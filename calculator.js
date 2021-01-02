daa1e0b3d3f32dab7bcbe632f2b656cc40ac16e4
courtsideKB <kbankhead1992.yahoo.com>
2021-01-02 00:12:13 -0800

    bmiCalculator and index file can pboth GET and POST from the calculator server.

--- calculator.js
+++ calculator.js
@@ -6,21 +6,29 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+/****** index.html GET and POST  methods ******/
+
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/bmiCalculator.html");
+    res.sendFile(__dirname + "/index.html");
 });
 
-/* app.post("/", (req, res) => {
+app.post("/", (req, res) => {
     var num1 = Number(req.body.num1);
     var num2 = Number(req.body.num2);
 
     var result = num1 + num2;
 
     res.send("The result of the calculation is " + result);
-}); */
+});
 
-app.post("/", (req, res) => {
-    var height = Number(req.body.height);
+/****** bmiCalculator.html GET and POST methods  ******/
+
+app.get("/bmiCalculator", (req, res) => {
+    res.sendFile(__dirname + "/bmiCalculator.html");
+})
+
+app.post("/bmiCalculator", (req, res) => {
+    var height = parseFloat(req.body.height);
     var weight = Number(req.body.weight);
 
     var bmi = Math.round(weight / Math.pow(height, 2));
